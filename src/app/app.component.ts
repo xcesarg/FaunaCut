8ca52651d9a033a7ffb1b76035ac460cc883d7b0
Raledro <cubirafa21@gmail.com>
2025-03-04 13:32:24 -0600

    Agregando foorer como componente de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-
-}
+export class AppComponent { }
