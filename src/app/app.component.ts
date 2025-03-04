5409fde2b394809627efc2341e09553a9a10e5d3
Raledro <cubirafa21@gmail.com>
2025-03-04 13:53:43 -0600

    estilizando footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent], 
+  imports: [RouterOutlet, FooterComponent], //parece que hay que ir añadiendo los componentes acá para que se muestren en la página
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
